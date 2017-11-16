0e965ead4a986975d8eee364d79e9f4c9193b223
Alice Freda <alicefreda@gmail.com>
2017-11-16 00:24:06 +0000

    Done.

--- question.js
+++ question.js
@@ -1,1 +1,24 @@
-'use strict';
+class Question {
+  constructor(content) {
+    this.content = content;
+    // this.id = ++Question.counter;
+    this.save();
+  }
+  save() {
+    this.constructor._All.push(this);
+  }
+  static All() {
+    return this._All;
+  }
+  // static Find(id) {
+  //   return this.All().filter(function(content){
+  //     return content.id === id;
+  //   })
+  // }
+  static Find(id) {
+    return this.All()[id - 1];
+  }
+}
+
+Question._All = [];
+// Question.counter = 0;
